5c441404004504ee48e60e9afc29317196dd2d49
Tuan <tuan69878@gmail.com>
2022-01-05 13:47:11 +0700

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var i =0;
-var images=[];
-var time=3000;
+var i =0; //starting point
+var images=[]; // khai bao mang
+var time=3000; // 3s
 
 images[0] ='a_1.png';
 images[1] ='a_2.png';
@@ -23,3 +23,15 @@ function changeImg()
 }
 
 window.onload = changeImg;
+
+$(document).ready(function(){
+    $(".promotion-pic").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><ion-icon name='chevron-back'></ion-icon></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><ion-icon name='chevron-forward'></ion-icon></button>",
+    });
+  });
